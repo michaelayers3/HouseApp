4d48a0b52b80c4042a4fbecfa98087f72f3cdf80
Michael <michaelgregoryayers@gmail.com>
2024-05-29 16:21:01 -0500

    working on the room page. looking okay!

---
+++ roompage.js
@@ -0,0 +1,23 @@
+// okay so we need some logic for the following
+
+
+// populate the room name based on a list//
+
+
+
+//back button, home button functional//
+
+
+
+//list is can change colors, scrollable, loaded from different document//
+
+
+
+//notes section is usable//
+
+
+
+//save button is usable//
+
+
+
